65bb99bd924c9af4f6b847662e534f09a601c0d9
Luka Stopar <luka.stopar@ijs.si>
2017-10-25 17:35:11 +0200

    - generating DMoz structure file - added description to DMoz topic

--- medline_classifier/src/structs/structs.py
+++ medline_classifier/src/structs/structs.py
@@ -57,24 +57,28 @@ class DMozPage:
 
 class DMozTopic:
 
-    def __init__(self, topic_id, path_name, output_name, pages):
+    def __init__(self, topic_id, title, path_name, output_name, description):
         self.topic_id = str(topic_id)
+        self.title = title
         self.path_name = path_name
         self.output_name = output_name
-        self.pages = pages
+        self.description = description
+        self.pages = []
 
         self._subtopic_map = {}
 
     def addSubtopic(self, subtopic):
-        self._subtopic_map[subtopic.path_name] = subtopic
+        self._subtopic_map[subtopic.title] = subtopic
 
-    def getSubtopicByName(self, path_name):
-        return self._subtopic_map[path_name]
+    def getSubtopicByTitle(self, title):
+        if title not in self._subtopic_map:
+            raise ValueError('Subtopic ' + title + ' doesn\'t exist!')
+        return self._subtopic_map[title]
 
     def addPage(self, page):
         self.pages.append(page)
 
-    def writeToXml(self, root):
+    def writeContentToXml(self, root):
         topic_el = etree.Element('Topic')
         topic_el.set(DMOZ_RDF + 'id', self.output_name)
 
@@ -99,45 +103,87 @@ class DMozTopic:
             root.append(page_el)
 
         for subtopic in self._subtopic_map.values():
-            subtopic.writeToXml(root)
+            subtopic.writeContentToXml(root)
+
+    def writeStructureToXml(self, root):
+        should_write = len(self.pages) > 0
+        wrote_child_arr = []
+        # write all the subtopics
+        for subtopic in self._subtopic_map.values():
+            wrote_child = subtopic.writeStructureToXml(root)
+            should_write |= wrote_child
+            wrote_child_arr.append(wrote_child)
+
+        # if we wrote at least one subtopic then insert yourself at the begining
+        if should_write:
+            topic_el = etree.Element('Topic')
+            topic_el.set(DMOZ_RDF + 'id', self.output_name)
+
+            catid_el = etree.Element('catid')
+            catid_el.text = self.topic_id
+            topic_el.append(catid_el)
+
+            title_el = etree.Element(DMOZ_ELEMENTS + 'Title', nsmap=DMOZ_NS_MAP)
+            title_el.text = self.title
+            topic_el.append(title_el)
+
+            desc_el = etree.Element(DMOZ_ELEMENTS + 'Description', nsmap=DMOZ_NS_MAP)
+            desc_el.text = self.description if self.description is not None else ''
+            topic_el.append(desc_el)
+
+            for subtopicN, subtopic in enumerate(self._subtopic_map.values()):
+                if not wrote_child_arr[subtopicN]:
+                    continue
+                narrow_el = etree.Element('narrow')
+                narrow_el.set(DMOZ_RDF + 'resource', subtopic.path_name)
+                narrow_el.text = ''
+                topic_el.append(narrow_el)
+            # insert yourself at the beginning
+            root.insert(0, topic_el)
+
+        # return whether we wrote ourself
+        return should_write
 
 
 class DMozOntology:
 
     def __init__(self):
         self._topic_map = {}
-        self._root_topic = DMozTopic(1, '', '', [])
+        self._root_topic = DMozTopic(1, '', '', '', '')
 
-        self._root_topic.addSubtopic(DMozTopic(2, 'Top', 'Top/World', []))
+        self._root_topic.addSubtopic(DMozTopic(2, 'Top', 'Top', 'Top/World', ''))
 
     def addPage(self, page):
         topic_id = page.topic_id
         topic_name = page.topic_name
 
         if not self.topicExists(topic_id):
-            raise ValueError('Topic `' + topic_name + '` does not exist!')
-            # self._createTopic(topic_id, topic_name)
+            raise ValueError('Topic `' + topic_name + '` with ID `' + topic_id + '` does not exist!')
 
         topic = self.getTopic(topic_id)
+        if topic.output_name != topic_name:
+            raise ValueError('Invalid name of topic with ID `' + topic_id + '` should be `' + topic.output_name + '` but page has: ' + page.topic_name)
+
         topic.addPage(page)
 
     def getTopic(self, topic_id):
-        return self._topic_map[topic_id]
+        return self._topic_map[str(topic_id)]
 
     def topicExists(self, topic_id):
-        return topic_id in self._topic_map
+        return str(topic_id) in self._topic_map
 
-    def defineTopic(self, topic_id, topic_name):
+    def defineTopic(self, topic_id, topic_name, description):
         if self.topicExists(topic_id):
             return
 
+        topic_id = str(topic_id)
         topic_path = topic_name.split('/')
 
         parent_topic = self._root_topic
-        for name_part in topic_path[0:-1]:
-            parent_topic = parent_topic.getSubtopicByName(name_part)
+        for subtopic_title in topic_path[0:-1]:
+            parent_topic = parent_topic.getSubtopicByTitle(subtopic_title)
 
-        topic = DMozTopic(topic_id, topic_path[-1], topic_name, [])
+        topic = DMozTopic(topic_id, topic_path[-1], topic_name, topic_name, description)
         parent_topic.addSubtopic(topic)
         self._topic_map[topic_id] = topic
 
@@ -145,7 +191,16 @@ class DMozOntology:
         root = etree.Element('RDF', nsmap=DMOZ_NS_MAP)
 
         topic = self._root_topic
-        topic.writeToXml(root)
+        topic.writeContentToXml(root)
+
         # for topic in self.topics:
-        #     topic.writeToXml(root)
+        #     topic.writeContentToXml(root)
+        return root
+
+    def getStructureXml(self):
+        root = etree.Element('RDF', nsmap=DMOZ_NS_MAP)
+
+        topic = self._root_topic
+        topic.writeStructureToXml(root)
+
         return root
--- medline_classifier/test/test-dmoz.py
+++ medline_classifier/test/test-dmoz.py
@@ -17,10 +17,10 @@ class TestDMozGenerator(unittest.TestCase):
         # topic = structs.DMozTopic(topic_id, topic_name, [])
         ontology = structs.DMozOntology()
 
-        ontology.defineTopic(3, 'Top/Bla')
-        ontology.defineTopic(4, 'Top/Bla/Bla')
-        ontology.defineTopic(5, 'Top/Bla/Bla/Hopsasa')
-        ontology.defineTopic(topic_id, topic_name)
+        ontology.defineTopic(3, 'Top/Bla', 'description-3')
+        ontology.defineTopic(4, 'Top/Bla/Bla', 'description-4')
+        ontology.defineTopic(5, 'Top/Bla/Bla/Hopsasa', 'description-5')
+        ontology.defineTopic(topic_id, topic_name, 'description-' + topic_id)
 
         for page_n in range(5):
             page = structs.DMozPage(
@@ -35,7 +35,7 @@ class TestDMozGenerator(unittest.TestCase):
         root = ontology.getContentXml()
 
         expected_str = \
-           '<RDF xmlns:r="http://www.w3.org/TR/RDF/" xmlns:d="http://purl.org/dc/elements/1.0/" xmlns="http://dmoz.org/rdf/">' + \
+           '<RDF xmlns:d="http://purl.org/dc/elements/1.0/" xmlns:r="http://www.w3.org/TR/RDF/" xmlns="http://dmoz.org/rdf/">' + \
             '<Topic r:id="">' + \
              '<catid>1</catid>' + \
             '</Topic>' + \
@@ -84,25 +84,91 @@ class TestDMozGenerator(unittest.TestCase):
             '</ExternalPage>' + \
            '</RDF>'
 
-        print etree.tostring(root, pretty_print=True)
+        # print etree.tostring(root, pretty_print=True)
 
         expected_root = etree.fromstring(expected_str)
         self.assertEqual(etree.tostring(root), etree.tostring(expected_root))
 
     def testStructureFile(self):
-        pass
+        ontology = structs.DMozOntology()
+
+        ontology.defineTopic(3, 'Top/Pages', 'topic-3')
+        ontology.defineTopic(4, 'Top/Utils', 'topic-4')
+        ontology.defineTopic(5, 'Top/Pages/Book', 'topic-5')
+        ontology.defineTopic(6, 'Top/Pages/Html', 'topic-6')
+        ontology.defineTopic(7, 'Top/Pages/Web', 'topic-7')    # should not appear in the output because it is empty
+        ontology.defineTopic(8, 'Top/Utils/Web', 'topic-8')
+
+        page1 = structs.DMozPage('url-1', 'title-1', 'description-1', 5, 'Top/Pages/Book')
+        page2 = structs.DMozPage('url-2', 'title-2', 'description-2', 5, 'Top/Pages/Book')
+        page3 = structs.DMozPage('url-3', 'title-3', 'description-3', 6, 'Top/Pages/Html')
+        page4 = structs.DMozPage('url-4', 'title-4', 'description-4', 8, 'Top/Utils/Web')
+
+        ontology.addPage(page1)
+        ontology.addPage(page2)
+        ontology.addPage(page3)
+        ontology.addPage(page4)
+
+        root = ontology.getStructureXml()
+
+        expected_str = \
+            '<RDF xmlns:d="http://purl.org/dc/elements/1.0/" xmlns:r="http://www.w3.org/TR/RDF/" xmlns="http://dmoz.org/rdf/">' + \
+              '<Topic r:id="">' + \
+                  '<catid>1</catid>' + \
+                  '<d:Title></d:Title>' + \
+                  '<d:Description></d:Description>' + \
+                  '<narrow r:resource="Top"></narrow>' + \
+              '</Topic>' + \
+              '<Topic r:id="Top/World">' + \
+                  '<catid>2</catid>' + \
+                  '<d:Title>Top</d:Title>' + \
+                  '<d:Description></d:Description>' + \
+                  '<narrow r:resource="Top/Utils"></narrow>' + \
+                  '<narrow r:resource="Top/Pages"></narrow>' + \
+              '</Topic>' + \
+              '<Topic r:id="Top/Pages">' + \
+                  '<catid>3</catid>' + \
+                  '<d:Title>Pages</d:Title>' + \
+                  '<d:Description>topic-3</d:Description>' + \
+                  '<narrow r:resource="Top/Pages/Html"></narrow>' + \
+                  '<narrow r:resource="Top/Pages/Book"></narrow>' + \
+              '</Topic>' + \
+              '<Topic r:id="Top/Pages/Book">' + \
+                  '<catid>5</catid>' + \
+                  '<d:Title>Book</d:Title>' + \
+                  '<d:Description>topic-5</d:Description>' + \
+              '</Topic>' + \
+              '<Topic r:id="Top/Pages/Html">' + \
+                  '<catid>6</catid>' + \
+                  '<d:Title>Html</d:Title>' + \
+                  '<d:Description>topic-6</d:Description>' + \
+              '</Topic>' + \
+              '<Topic r:id="Top/Utils">' + \
+                  '<catid>4</catid>' + \
+                  '<d:Title>Utils</d:Title>' + \
+                  '<d:Description>topic-4</d:Description>' + \
+                  '<narrow r:resource="Top/Utils/Web"></narrow>' + \
+              '</Topic>' + \
+              '<Topic r:id="Top/Utils/Web">' + \
+                  '<catid>8</catid>' + \
+                  '<d:Title>Web</d:Title>' + \
+                  '<d:Description>topic-8</d:Description>' + \
+              '</Topic>' + \
+            '</RDF>'
+
+        self.assertEqual(etree.tostring(root), expected_str)
 
     def testConstruction(self):
         ontology = structs.DMozOntology()
 
-        ontology.defineTopic(3, 'Top/Pages')
-        ontology.defineTopic(4, 'Top/Girlscouts')
-        ontology.defineTopic(5, 'Top/Pages/Tralala')
-        ontology.defineTopic(6, 'Top/Girlscouts/Hopsasa')
+        ontology.defineTopic(3, 'Top/Pages', 'description-3')
+        ontology.defineTopic(4, 'Top/Girlscouts', 'description-4')
+        ontology.defineTopic(5, 'Top/Pages/Tralala', 'description-5')
+        ontology.defineTopic(6, 'Top/Girlscouts/Hopsasa', 'description-6')
 
-        page1 = structs.DMozPage('url-1', 'title-1', 'description-1', 7, 'Top/Pages/Tralala')
-        page2 = structs.DMozPage('url-2', 'title-2', 'description-2', 7, 'Top/Pages/Tralala')
-        page3 = structs.DMozPage('url-3', 'title-3', 'description-3', 8, 'Top/Girlscouts/Hopsasa')
+        page1 = structs.DMozPage('url-1', 'title-1', 'description-1', 5, 'Top/Pages/Tralala')
+        page2 = structs.DMozPage('url-2', 'title-2', 'description-2', 5, 'Top/Pages/Tralala')
+        page3 = structs.DMozPage('url-3', 'title-3', 'description-3', 6, 'Top/Girlscouts/Hopsasa')
 
         ontology.addPage(page1)
         ontology.addPage(page2)
@@ -110,13 +176,21 @@ class TestDMozGenerator(unittest.TestCase):
 
         root = ontology.getContentXml()
 
+        # print etree.tostring(root, pretty_print=True)
+
         # TODO should the "Top" topic be here???
         expected_str = \
-           '<RDF xmlns:r="http://www.w3.org/TR/RDF/" xmlns:d="http://purl.org/dc/elements/1.0/" xmlns="http://dmoz.org/rdf/">' + \
+           '<RDF xmlns:d="http://purl.org/dc/elements/1.0/" xmlns:r="http://www.w3.org/TR/RDF/" xmlns="http://dmoz.org/rdf/">' + \
             '<Topic r:id="">' + \
              '<catid>1</catid>' + \
             '</Topic>' + \
+            '<Topic r:id="Top/World">' + \
+             '<catid>2</catid>' + \
+            '</Topic>' + \
             \
+            '<Topic r:id="Top/Pages">' + \
+             '<catid>3</catid>' + \
+            '</Topic>' + \
             '<Topic r:id="Top/Pages/Tralala">' + \
              '<catid>5</catid>' + \
              '<link r:resource="url-1"></link>' + \
@@ -134,6 +208,9 @@ class TestDMozGenerator(unittest.TestCase):
              '<topic>Top/Pages/Tralala</topic>' + \
             '</ExternalPage>' + \
             \
+            '<Topic r:id="Top/Girlscouts">' + \
+             '<catid>4</catid>' + \
+            '</Topic>' + \
             '<Topic r:id="Top/Girlscouts/Hopsasa">' + \
              '<catid>6</catid>' + \
              '<link r:resource="url-3"></link>' + \
@@ -151,12 +228,16 @@ class TestDMozGenerator(unittest.TestCase):
 
     def testExceptNonExistTopic(self):
         ontology = structs.DMozOntology()
-        ontology.defineTopic(3, 'Top/Pages')
+        ontology.defineTopic(3, 'Top/Pages', 'description-3')
 
         with self.assertRaises(ValueError):
-            structs.DMozPage('url-4', 'title-4', 'description-4', 5, 'Top/Girlscouts/Hopsasa')
+            ontology.defineTopic(4, 'Top/Bla/Hop', 'description-4')
         with self.assertRaises(ValueError):
-            structs.DMozPage('url-1', 'title-1', 'description-1', 3, 'Top/Pages/Tralala')
+            page = structs.DMozPage('url-4', 'title-4', 'description-4', 5, 'Top/Girlscouts/Hopsasa')
+            ontology.addPage(page)
+        with self.assertRaises(ValueError):
+            page = structs.DMozPage('url-1', 'title-1', 'description-1', 3, 'Top/Pages/Tralala')
+            ontology.addPage(page)
 
 
 if __name__ == '__main__':
